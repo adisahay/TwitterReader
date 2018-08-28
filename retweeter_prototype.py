1d621eb90ff5a6912a5e3d426135c6e5e98429aa
Aditya Sahay <adisahay@gmail.com>
2018-08-28 09:51:40 -0400

    Code refactoring, added some logs.

--- retweeter_prototype.py
+++ retweeter_prototype.py
@@ -1,21 +1,34 @@
 import twitter, csv, unicodedata
 
-api = twitter.Api(consumer_key="", # Provide Consumer Key
-                consumer_secret="", # Provide Consumer Secret
-                access_token_key="", # Provide Access Token Key
-                access_token_secret="") # Provide Access Token Secret
+filename = "locations.csv"
+
+# Twitter developer credentials
+consumer_key="", # Provide Consumer Key
+consumer_secret="", # Provide Consumer Secret
+access_token_key="", # Provide Access Token Key
+access_token_secret="" # Provide Access Token Secret
+
+api = twitter.Api(consumer_key=consumer_key,
+                consumer_secret=consumer_secret,
+                access_token_key=access_token_key,
+                access_token_secret=access_token_secret)
 
 tweetId = 1032804122330845184 # Replace the tweet ID value with the actual one
 
-csvfile = open("locations.csv", "wb")
+csvfile = open(filename, "wb")
 writer = csv.writer(csvfile)
 writer.writerow(["Locations"]) # Set the header row
 
+print ("Fetching retweeters for Tweet ID: " + str(tweetId))
 retweeters = api.GetRetweeters(tweetId) # Fetch the retweeters for this tweet
 
+print ("Getting retweeters location to CSV file: " + filename)
 for user in list(retweeters):
     user_info = api.GetUser(user)
-    # Filter out those which do not have locations
+    # Filter out empty location values
     if user_info.location != "":
-        asciiString = unicodedata.normalize('NFKD', user_info.location).encode('ascii','ignore')
+        normalized = unicodedata.normalize('NFKD', user_info.location)
+        asciiString = normalized.encode('ascii','ignore')
         writer.writerow([asciiString])
+
+print ("Finished writing to CSV file.")
