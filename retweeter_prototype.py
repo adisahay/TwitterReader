09f4039ba78dfb5eb24c4c30ccd95e39a353fc46
Gourab Mitra <gourabmi@buffalo.edu>
2018-08-28 22:10:21 -0700

    edits

--- retweeter_prototype.py
+++ retweeter_prototype.py
@@ -1,8 +1,9 @@
 import twitter, csv, unicodedata
 
-filename = "locations.csv"
+filename = "locations.csv" # TODO Could you provide a sample output of locations.csv in the repositoty ?
 
 # Twitter developer credentials
+# TODO Fetch these credentials from input_twitter_api_credentials.csv . Perhaps use https://www.pythonforbeginners.com/files/with-statement-in-python
 consumer_key="", # Provide Consumer Key
 consumer_secret="", # Provide Consumer Secret
 access_token_key="", # Provide Access Token Key
@@ -13,9 +14,10 @@ api = twitter.Api(consumer_key=consumer_key,
                 access_token_key=access_token_key,
                 access_token_secret=access_token_secret)
 
+# TODO Fetch a list of tweet ids and run the 'getRetweeters' for each of them
 tweetId = 1032804122330845184 # Replace the tweet ID value with the actual one
 
-csvfile = open(filename, "wb")
+csvfile = open(filename, "wb") # TODO Possible dangling file handles or unhandled exceptions ? Use the Python With statement ( http://effbot.org/zone/python-with-statement.htm ) for auto-management of file resources.
 writer = csv.writer(csvfile)
 writer.writerow(["Locations"]) # Set the header row
 
