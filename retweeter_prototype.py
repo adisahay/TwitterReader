fde699fd53e423562c256db5b0ec91e3bb0f6e55
Aditya Sahay <adisahay@gmail.com>
2018-09-01 23:58:21 -0400

    Reading credentials from CSV file. Using "with" to open files. Added the locations.csv output file for the sample tweet.

--- retweeter_prototype.py
+++ retweeter_prototype.py
@@ -1,36 +1,44 @@
-import twitter, csv, unicodedata
+import twitter, csv
 
-filename = "locations.csv" # TODO Could you provide a sample output of locations.csv in the repositoty ?
+filename = "locations.csv"
 
 # Twitter developer credentials
-# TODO Fetch these credentials from input_twitter_api_credentials.csv . Perhaps use https://www.pythonforbeginners.com/files/with-statement-in-python
-consumer_key="", # Provide Consumer Key
-consumer_secret="", # Provide Consumer Secret
-access_token_key="", # Provide Access Token Key
-access_token_secret="" # Provide Access Token Secret
+consumer_key = "",
+consumer_secret = "",
+access_token_key = "",
+access_token_secret = ""
 
-api = twitter.Api(consumer_key=consumer_key,
-                consumer_secret=consumer_secret,
-                access_token_key=access_token_key,
-                access_token_secret=access_token_secret)
+with open("input_twitter_api_credentials.csv") as credfile:
+    credentials = csv.reader(credfile, delimiter=",")
+    rownum = 0
+    for row in credentials:
+        if rownum == 1:
+            consumer_key = row[0],
+            consumer_secret = row[1],
+            access_token_key = row[2],
+            access_token_secret = row[3]
+        rownum += 1
+
+api = twitter.Api(consumer_key="".join(consumer_key),
+                consumer_secret="".join(consumer_secret),
+                access_token_key="".join(access_token_key),
+                access_token_secret="".join(access_token_secret))
 
 # TODO Fetch a list of tweet ids and run the 'getRetweeters' for each of them
 tweetId = 1032804122330845184 # Replace the tweet ID value with the actual one
 
-csvfile = open(filename, "wb") # TODO Possible dangling file handles or unhandled exceptions ? Use the Python With statement ( http://effbot.org/zone/python-with-statement.htm ) for auto-management of file resources.
-writer = csv.writer(csvfile)
-writer.writerow(["Locations"]) # Set the header row
+with open(filename, "w") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Locations"]) # Set the header row
 
-print ("Fetching retweeters for Tweet ID: " + str(tweetId))
-retweeters = api.GetRetweeters(tweetId) # Fetch the retweeters for this tweet
+    print ("Fetching retweeters for Tweet ID: " + str(tweetId))
+    retweeters = api.GetRetweeters(tweetId) # Fetch the retweeters for this tweet
 
-print ("Getting retweeters location to CSV file: " + filename)
-for user in list(retweeters):
-    user_info = api.GetUser(user)
-    # Filter out empty location values
-    if user_info.location != "":
-        normalized = unicodedata.normalize('NFKD', user_info.location)
-        asciiString = normalized.encode('ascii','ignore')
-        writer.writerow([asciiString])
+    print ("Getting retweeters location to CSV file: " + filename)
+    for user in list(retweeters):
+        user_info = api.GetUser(user)
+        # Filter out empty location values
+        if user_info.location != "":
+            writer.writerow([user_info.location])
 
 print ("Finished writing to CSV file.")
