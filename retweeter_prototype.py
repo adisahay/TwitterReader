3114737581731d3699c6989cf02a5c213cf261f2
Aditya Sahay <adisahay@gmail.com>
2018-09-09 16:40:40 -0400

    Reading tweet IDs from CSV file. Error handling for invalid IDs.

--- retweeter_prototype.py
+++ retweeter_prototype.py
@@ -1,6 +1,7 @@
 import twitter, csv
 
-filename = "locations.csv"
+inputFile = "input_tweetIds.csv"
+outputFile = "locations.csv"
 
 # Twitter developer credentials
 consumer_key = "",
@@ -9,6 +10,7 @@ access_token_key = "",
 access_token_secret = ""
 
 with open("input_twitter_api_credentials.csv") as credfile:
+    # Read only the second row and ignore the rest
     credentials = list(csv.reader(credfile, delimiter=","))[1:2][0]
     consumer_key = credentials[0]
     consumer_secret = credentials[1]
@@ -20,20 +22,32 @@ api = twitter.Api(consumer_key="".join(consumer_key),
                 access_token_key="".join(access_token_key),
                 access_token_secret="".join(access_token_secret))
 
-# TODO Fetch a list of tweet ids and run the 'getRetweeters' for each of them
-tweetIdList = [1032804122330845184, 1036461028429651968, 1036459095883227139]
+# Fetch a list of tweet ids and run the 'getRetweeters' for each of them
+# TBD: The list can be fetched from a file or by waiting for live tweets
+tweetIdList = []
+with open(inputFile, "r") as inputCSV:
+    tweets = list(csv.reader(inputCSV, delimiter=","))[1:]
+    tweets = [int(tweet[0]) for tweet in tweets]
+    tweetIdList = tweets
+
 statuses = api.GetStatuses(tweetIdList, include_entities=False, map=True)
 
-with open(filename, "w") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Handles", "Tweet IDs", "Retweeter Handles", "Locations"]) # Set the header row
+with open(outputFile, "w") as outputCSV:
+    writer = csv.writer(outputCSV)
+    # Prepare heading row
+    writer.writerow(["Handles", "Tweet IDs", "Retweeter Handles", "Locations"])
     found = 0
     totalCount = 0
     for tweetId in tweetIdList:
-        print ("Fetching retweeters for Tweet ID: " + str(tweetId))
-        retweeters = api.GetRetweeters(tweetId) # Fetch the retweeters for this tweet
+        # Error handling for invalid IDs
+        if statuses[tweetId] == None:
+            continue
 
-        print ("Getting retweeters location to CSV file: " + filename)
+        print ("Fetching retweeters for Tweet ID: " + str(tweetId))
+        # Fetch the retweeters for this tweet
+        retweeters = api.GetRetweeters(tweetId)
+
+        print ("Getting retweeters location to CSV file: " + outputFile)
         count = 1
         for user in list(retweeters):
             user_info = api.GetUser(user)
@@ -41,6 +55,8 @@ with open(filename, "w") as csvfile:
             if user_info.location != "":
                 writer.writerow([statuses[tweetId].AsDict()["user"]["screen_name"], tweetId, user_info.screen_name, user_info.location])
                 found += 1
+
+            # Update the percentage progress
             print("\r" + str(count * 100 // len(retweeters)) + "% Completed", end="", flush=True)
             count += 1
         totalCount += (len(retweeters))
